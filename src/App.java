b9e43be6169d68f00950581b2ad9763e4f522831
ibrahimbutun <ibrahimbutun@outlook.com>
2023-12-21 18:07:18 +0300

    ciftTekrar

---
+++ src/App.java
@@ -0,0 +1,33 @@
+public class App {
+
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] list = { 3, 7, 3, 3, 2, 2, 10, 21, 10, 6, 9, 10, 6 };
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if ((i != j) && (list[i] == list[j])) {
+                    if (!isFind(duplicate, list[i])) {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+        for (int value : duplicate) {
+            if ((value != 0) && (value % 2 == 0)) {
+                System.out.println(value);
+            }
+        }
+    }
+}
